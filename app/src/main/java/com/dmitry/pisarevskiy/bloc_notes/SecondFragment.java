e5eaf7a89f77f5a72c993b57944740082ddb204a
Dmitry <dmitrypisarevskiy@gmail.com>
2020-11-18 23:24:24 +0500

    initial commit

--- app/src/main/java/com/dmitry/pisarevskiy/bloc_notes/SecondFragment.java
+++ app/src/main/java/com/dmitry/pisarevskiy/bloc_notes/SecondFragment.java
@@ -28,7 +28,6 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
-
             }
         });
     }
